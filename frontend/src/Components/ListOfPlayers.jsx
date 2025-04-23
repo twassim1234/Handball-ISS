f5b9156e24c2857e28c2ead66bf4545e783b8c42
jed <jed@optipross.com>
2025-04-23 17:29:17 +0100

    initial commit

--- frontend/src/Components/ListOfPlayers.jsx
+++ frontend/src/Components/ListOfPlayers.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { UserContext } from "../Contexts/UserContext"; 
+import { UserContext } from "../Contexts/UserContext";
 import { useContext } from "react";
 import axios from "axios";
 
-const ListOfPlayers = ({club1_name, club2_name, club1_id, club2_id, match_id}) => {
+const ListOfPlayers = ({
+  club1_name,
+  club2_name,
+  club1_id,
+  club2_id,
+  match_id,
+}) => {
   const { user } = useContext(UserContext);
   const [club1_players, setClub1players] = useState([]);
   const [club2_players, setClub2players] = useState([]);
@@ -13,85 +19,88 @@ const ListOfPlayers = ({club1_name, club2_name, club1_id, club2_id, match_id}) =
 
   const handlePlayerAdd = async (player_id) => {
     try {
-      console.log(player_id)
+      console.log(player_id);
       await axios.post(`http://localhost:8000/match/player-performance`, {
         match_id: match_id,
         player_id: player_id,
-      })
-      const result = await fetchMatchPlayers()
-      setMatchPlayers(result.players)
+      });
+      const result = await fetchMatchPlayers();
+      setMatchPlayers(result.players);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
   const fetchPlayers = async (club_id) => {
-    const response = await axios.get(`http://localhost:8000/club/players/${club_id}`);
-    return response.data
-  }
+    const response = await axios.get(
+      `http://localhost:8000/club/players/${club_id}`
+    );
+    return response.data;
+  };
   const fetchMatchPlayers = async () => {
-    const response = await axios.get(`http://localhost:8000/match/players/${match_id}`);
-    return response.data
-  }
+    const response = await axios.get(
+      `http://localhost:8000/match/players/${match_id}`
+    );
+    return response.data;
+  };
 
   const getPlayer = (club_players, player_id) => {
-    return club_players.find(player => player.player_id === player_id)
-  }
+    return club_players.find((player) => player.player_id === player_id);
+  };
 
   const onPlayerPerformanceChange = async (player_id, path, value) => {
     try {
-      setMatchPlayers(match_players.map(player => {
-        if(player.player_id === player_id) {
-          return {
-            ...player,
-            [path]: value
+      setMatchPlayers(
+        match_players.map((player) => {
+          if (player.player_id === player_id) {
+            return {
+              ...player,
+              [path]: value,
+            };
           }
-        }
-        return player
-      }
-      ))
+          return player;
+        })
+      );
+    } catch (error) {
+      console.log(error);
     }
-    catch (error) {
-      console.log(error)
-    }
-  }
+  };
 
   const handlePlayerPerformanceSave = async (player_id) => {
     try {
-      const player = match_players.find(player => player.player_id === player_id)
-      await axios.put(`http://localhost:8000/match/performance`, player)
-    }
-    catch (error) {
-      console.log(error)
-    }
-  }
-
-  const handleDeletePlayer = async (player_id) =>{
-    try {
-      await axios.delete(`http://localhost:8000/match/player-performance/${match_id}/${player_id}`)
-      const result = await fetchMatchPlayers()
-      setMatchPlayers(result.players)
+      const player = match_players.find(
+        (player) => player.player_id === player_id
+      );
+      await axios.put(`http://localhost:8000/match/performance`, player);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
+  const handleDeletePlayer = async (player_id) => {
+    try {
+      await axios.delete(
+        `http://localhost:8000/match/player-performance/${match_id}/${player_id}`
+      );
+      const result = await fetchMatchPlayers();
+      setMatchPlayers(result.players);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    if(!user) return;
-    fetchPlayers(club1_id).then(data => setClub1players(data.players))
-    fetchPlayers(club2_id).then(data => setClub2players(data.players))
-    fetchMatchPlayers(match_id).then(data => setMatchPlayers(data.players))
-  }
-  ,[user])
-
-
+    if (!user) return;
+    fetchPlayers(club1_id).then((data) => setClub1players(data.players));
+    fetchPlayers(club2_id).then((data) => setClub2players(data.players));
+    fetchMatchPlayers(match_id).then((data) => setMatchPlayers(data.players));
+  }, [user]);
 
   return (
     <div>
-      <div className="flex space-x-4 mt-12">
+      <div className="flex justify-around mt-12">
         {/* Team A Table */}
         <div className="flex flex-col justify-start items-start">
-          <table className="w-1/2 border-collapse border">
+          <table className="w-2/5 border-collapse border">
             <thead>
               <tr>
                 <th
@@ -121,7 +130,9 @@ const ListOfPlayers = ({club1_name, club2_name, club1_id, club2_id, match_id}) =
                 )
                 .map((player, index) => (
                   <tr key={index}>
-                    <td className="border p-2 text-center">{index + 1}</td>
+                    <td className="border p-2 text-center">
+                      {getPlayer(club1_players, player.player_id).reference}
+                    </td>
                     <td className="border p-2 text-center">
                       <input type="text" className="w-16 p-1 border rounded" />
                     </td>
@@ -209,11 +220,17 @@ const ListOfPlayers = ({club1_name, club2_name, club1_id, club2_id, match_id}) =
                       <button
                         className="cursor-pointer"
                         title="save changes"
-                        onClick={() => handlePlayerPerformanceSave(player.player_id)}
+                        onClick={() =>
+                          handlePlayerPerformanceSave(player.player_id)
+                        }
                       >
                         💾
                       </button>
-                      <button className="cursor-pointer" title="delete" onClick={() => handleDeletePlayer(player.player_id)}>
+                      <button
+                        className="cursor-pointer"
+                        title="delete"
+                        onClick={() => handleDeletePlayer(player.player_id)}
+                      >
                         ✘
                       </button>
                     </td>
@@ -227,9 +244,14 @@ const ListOfPlayers = ({club1_name, club2_name, club1_id, club2_id, match_id}) =
               onChange={(e) => setClub1SelectedPlayer(e.target.value)}
             >
               <option value="">Select Player</option>
-              {club1_players.filter(player=>!match_players.find(p=>p.player_id===player.player_id)).map((player) => (
-                <option value={player.player_id}>{player.player_name}</option>
-              ))}
+              {club1_players
+                .filter(
+                  (player) =>
+                    !match_players.find((p) => p.player_id === player.player_id)
+                )
+                .map((player) => (
+                  <option value={player.player_id}>{player.player_name}</option>
+                ))}
             </select>
             <button
               className="bg-blue-600 text-white p-2 rounded cursor-pointer"
@@ -241,14 +263,10 @@ const ListOfPlayers = ({club1_name, club2_name, club1_id, club2_id, match_id}) =
         </div>
 
         {/* Sign Section between Teams */}
-        <div className="flex flex-col justify-center items-center w-1/2">
-          <div className="text-center font-bold text-xl p-4">
-            <p>VS</p> {/* This is the sign between the teams */}
-          </div>
-        </div>
+        <div className="flex flex-col justify-center items-center w-1/5"></div>
         {/* Team B Table */}
         <div className="flex flex-col justify-start items-start">
-          <table className="w-1/2 border-collapse border">
+          <table className="w-2/5 border-collapse border">
             <thead>
               <tr>
                 <th
@@ -278,7 +296,9 @@ const ListOfPlayers = ({club1_name, club2_name, club1_id, club2_id, match_id}) =
                 )
                 .map((player, index) => (
                   <tr key={index}>
-                    <td className="border p-2 text-center">{index + 1}</td>
+                    <td className="border p-2 text-center">
+                      {getPlayer(club2_players, player.player_id).reference}
+                    </td>
                     <td className="border p-2 text-center">
                       <input type="text" className="w-16 p-1 border rounded" />
                     </td>
@@ -366,11 +386,17 @@ const ListOfPlayers = ({club1_name, club2_name, club1_id, club2_id, match_id}) =
                       <button
                         className="cursor-pointer"
                         title="save changes"
-                        onClick={() => handlePlayerPerformanceSave(player.player_id)}
+                        onClick={() =>
+                          handlePlayerPerformanceSave(player.player_id)
+                        }
                       >
                         💾
                       </button>
-                      <button className="cursor-pointer" title="delete" onClick={() => handleDeletePlayer(player.player_id)}>
+                      <button
+                        className="cursor-pointer"
+                        title="delete"
+                        onClick={() => handleDeletePlayer(player.player_id)}
+                      >
                         ✘
                       </button>
                     </td>
@@ -384,9 +410,14 @@ const ListOfPlayers = ({club1_name, club2_name, club1_id, club2_id, match_id}) =
               onChange={(e) => setClub2SelectedPlayer(e.target.value)}
             >
               <option value="">Select Player</option>
-              {club2_players.filter(player=>!match_players.find(p=>p.player_id===player.player_id)).map((player) => (
-                <option value={player.player_id}>{player.player_name}</option>
-              ))}
+              {club2_players
+                .filter(
+                  (player) =>
+                    !match_players.find((p) => p.player_id === player.player_id)
+                )
+                .map((player) => (
+                  <option value={player.player_id}>{player.player_name}</option>
+                ))}
             </select>
             <button
               className="bg-blue-600 text-white p-2 rounded cursor-pointer"
@@ -396,8 +427,6 @@ const ListOfPlayers = ({club1_name, club2_name, club1_id, club2_id, match_id}) =
             </button>
           </div>
         </div>
-
-      
       </div>
 
       {/* Additional tables for other players or sections */}
--- frontend/src/Components/Pages/About.jsx
+++ frontend/src/Components/Pages/About.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
-
+import React from "react";
+import { motion } from "framer-motion";
+import Logo from "../../Assets/FTHBLogo.png";
+import Testimonials from "../../Components/Testimonials.jsx";
 const About = () => {
   return (
     <div>
-      hi hi hi
-    </div>
-  )
-}
+      <motion.div
+        initial={{ opacity: 0, x: 200 }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true }}
+        className="flex flex-col items-center justify-center container mt-40 mb-28
+    mx-auto sm:p-4 lg:px-32  w-full overflow-hidden"
+        id="About"
+      >
+        <h1 className="text-2xl sm:text-4xl font-bold mb-2">
+          About
+          <span
+            className="underline underline-offset-4 decoration-1 under
+         font-light"
+          >
+            {" "}
+            US
+          </span>{" "}
+        </h1>
 
-export default About
+        <p
+          className="text-gray-500 max-w-80 text-center 
+      mb-8"
+        >
+          Passionate About Sport, Dedicated to Your Vision{" "}
+        </p>
+
+        <div className="flex flex-col md:flex-row items-center md:items-start md:gap-14">
+          <img src={Logo} alt="logo" className="w-full sm:w-1/2 max-w-lg" />
+
+          <div className="flex flex-col  items-center md:items-start mt-10 text-gray-600">
+            <div className="grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28">
+              <div>
+                <p className="text-4xl font-medium text-gray-800">10+</p>
+                <p>Leagues</p>
+              </div>
+              <div>
+                <p className="text-4xl font-medium text-gray-800">12+</p>
+                <p>stadium</p>
+              </div>
+              <div>
+                <p className="text-4xl font-medium text-gray-800">200+</p>
+                <p>Team</p>
+              </div>
+              <div>
+                <p className="text-4xl font-medium text-gray-800">2500+</p>
+                <p>Players</p>
+              </div>
+            </div>
+            <p className="my-10 md:min-w-80 max-w-lg">
+              {" "}
+              The Handball Federation of Tunisia (Fédération Tunisienne de
+              Handball - FTHB) is the official governing body for the sport of
+              handball in Tunisia. Established in 1957, it oversees the
+              development, promotion, and regulation of handball at all levels
+              across the country. The federation is responsible for organizing
+              national competitions, managing the national teams, and
+              representing Tunisia in international tournaments. With a rich
+              history of achievements in Africa and beyond, the FTHB plays a key
+              role in shaping the future of handball in Tunisia.
+            </p>
+          </div>
+        </div>
+      </motion.div>
+      <Testimonials/>
+    </div>
+  );
+};
+
+export default About;
